76873525dd58d4634980d16a066c8e7fb4998aa8
m-ali422-a <mali0004222@gmail.com>
2026-07-29 22:02:57 +0500

    push the navbar code

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,105 @@
+import { Link, NavLink } from "react-router-dom";
+import { assets } from "../assets/assets";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div className="flex justify-between items-center p-4">
+      {/* Logo  */}
+      <img src={assets.logo} className="w-50" alt="" />
+
+      {/* Menu Buttons  */}
+      <ul className="sm:flex text-gray-700 text-m font-semibold gap-5 hidden">
+        <NavLink to="/" className="flex flex-col items-center gap-1 uppercase">
+          <p>Home</p>
+          <hr className="w-2/4 bg-gray-700 h-[1.5px] hidden" />
+        </NavLink>
+
+        <NavLink
+          to="/collection"
+          className="flex flex-col items-center gap-1 uppercase"
+        >
+          <p>Collection</p>
+          <hr className="w-2/4 bg-gray-700 h-[1.5px] hidden" />
+        </NavLink>
+
+        <NavLink
+          to="/about"
+          className="flex flex-col items-center gap-1 uppercase"
+        >
+          <p>About</p>
+          <hr className="w-2/4 bg-gray-700 h-[1.5px] hidden" />
+        </NavLink>
+
+        <NavLink
+          to="/contact"
+          className="flex flex-col items-center gap-1 uppercase"
+        >
+          <p>Contact</p>
+          <hr className="w-2/4 bg-gray-700 h-[1.5px] hidden" />
+        </NavLink>
+      </ul>
+
+      {/* Right Buttons  */}
+      <div className="flex items-center gap-7">
+        <img src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
+
+        <div className="group relative">
+          <img
+            src={assets.profile_icon}
+            className="w-5 cursor-pointer"
+            alt=""
+          />
+          <div className="group-hover:block hidden absolute right-0 py-2">
+            <div className="flex flex-col gap-2 px-3 py-2 w-30 text-gray-500 bg-slate-100 rounded">
+              <p className="cursor-pointer hover:text-black">My Profile</p>
+              <p className="cursor-pointer hover:text-black">Orders</p>
+              <p className="cursor-pointer hover:text-black">Logout</p>
+            </div>
+          </div>
+        </div>
+        <Link to="/cart" className="relative">
+          <img src={assets.cart_icon} className="w-5 cursor-pointer" alt="" />
+          <p className="absolute w-4 bg-black text-white text-center -right-1.25 -bottom-1.25 text-[9px] rounded-full leading-4">
+            10
+          </p>
+        </Link>
+        {/* Menu for icon small screens  */}
+        <img
+          onClick={() => setVisible(true)}
+          src={assets.menu_icon}
+          className="w-5 cursor-pointer sm:hidden"
+          alt=""
+        />
+      </div>
+
+      {/* Menu for small screens  */}
+      <div
+        className={`absolute top-0 right-0 overflow-hidden transition-all bg-white ${visible ? "w-full" : "w-0"} `}
+      >
+        <div className="flex flex-col text-gray-500">
+          <div
+            onClick={() => setVisible(false)}
+            className="flex items-center gap-3 p-4"
+          >
+            <img
+              src={assets.dropdown_icon}
+              className="h-4 rotate-180 cursor-pointer"
+              alt=""
+            />
+            <p className="text-[20px] cursor-pointer">Back</p>
+          </div>
+          <NavLink onClick={()=>setVisible(false)} to="/" className="py-2 pl-8 border-b font-semibold text-2xl">Home</NavLink>
+          <NavLink onClick={()=>setVisible(false)} to="/collection" className="py-2 pl-8 border-b font-semibold text-2xl">Collection</NavLink>
+          <NavLink onClick={()=>setVisible(false)} to="/about" className="py-2 pl-8 border-b font-semibold text-2xl">About</NavLink>
+          <NavLink onClick={()=>setVisible(false)} to="/contact" className="py-2 pl-8 border-b font-semibold text-2xl">Contact</NavLink>
+
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
